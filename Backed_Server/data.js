735cf62d40e53bf6b6d512a59d98c91927ef7226
MUKILAN019 <mukilan.p@kalvium.community>
2024-05-17 15:48:09 +0530

    added entities

---
+++ Backed_Server/data.js
@@ -0,0 +1,85 @@
+[
+        {
+                "name": "All India Institute of Medical Sciences (AIIMS), Delhi",
+                "description": "AIIMS is a premier government-run medical institute in India that offers a wide range of medical facilities. The hospital has an accredited ambulance service, diagnostic facilities, medicine shop, and mediclaim is accepted. AIIMS has a wide range of departments, 1,300 beds, and a team of over 600 doctors.",
+                "surgery_name": "Minimal Invasive, Breast, Endocrine, Gastrointestinal, Vascular, etc.",
+                "city": "Ansari Nagar, Delhi",
+                "img": "https://static.theprint.in/wp-content/uploads/2020/07/AIIMS-2.jpg",
+                "rate": 5
+        },
+        {
+                "name": "Omandurar Hospital,Tamilnadu",
+                "description": "Tamil Nadu Government Multi-Super-Speciality Hospital is a 400-bed government-owned super-speciality hospital in Chennai, India. It is located at the Omandurar Government Estate on Anna Salai and was opened in February 2014",
+                "surgery_name": "Procedures,Angioplasty,Coronary angiography,Carotid endarterectomy,Pulmonary lobectomy, etc.",
+                "city": "Chennai, Tamilnadu",
+                "img": "https://www.shutterstock.com/shutterstock/videos/1104819279/thumb/1.jpg?ip=x480",
+                "rate": 4.5
+        },
+        {
+                "name": "Jawaharlal Institute of Postgraduate Medical Education and Research (JIPMER)",
+                "description": "It is an institution established for teaching, research and patient care in the area of health. JIPMER spreads over 192 acres. The institution houses one administrative block, one academic centre, one nursing college, seven hospital blocks, seven accessory services buildings and four residential complexes.",
+                "surgery_name": "Ophthalmology, Orthopaedics, Paediatric Surgery, Plastic Surgery, etc.",
+                "city": "Puducherry",
+                "img": "https://jipmer.edu.in/sites/default/files/jipmer-entrance.jpg",
+                "rate": 4.6
+        },
+        {
+                "name": "King Edward Memorial (KEM) Hospital",
+                "description": "The K.E.M. Hospital was the first Indian hospital housing the out-patient department within the main hospital building). The plans were submitted to W.A.Pite who had designed the Kings College Hospital in London and was then a leading authority on hospital construction.",
+                "surgery_name": "Pancreatic surgery, Endocrine Surgery,  Colorectal surgery,  Breast surgery, etc.",
+                "city": "Mumbai, Maharashtra",
+                "img": "https://th-i.thgim.com/public/news/cities/mumbai/addd0n/article25184732.ece/alternates/FREE_1200/11BMKEMHOSPITALSTORY",
+                "rate": 4.9
+        },
+        {
+                "name": "Post Graduate Institute of Medical Education and Research",
+                "description": "Postgraduate Institute of Medical Education and Research (PGIMER) is a public medical university in Chandigarh, India. It is an 'Institute of National Importance'. It has educational, medical research, and training facilities for its students including all specialties, super specialties and sub specialties.",
+                "surgery_name": "IVF treatment, Cancer treatment, Robotic surgery , Pancreas surgery, etc.",
+                "city": "Chandigarh",
+                "img": "https://media.licdn.com/dms/image/C511BAQHwyxZHI72nEw/company-background_10000/0/1583987171033/postgraduate_institute_of_medical_education_and_research_cover?e=2147483647&v=beta&t=MLeIBVVqWImk7vv8S0L8V_iGqK540r1A5Fz4Wkcb73c",
+                "rate": 4.9
+        },
+        {
+                "name": "Safdarjung Hospital",
+                "description": "Safdarjung Hospital is the largest hospital under the Ministry of Health and Family Welfare and is well known for its quality of services to all strata of society since pre-independence days. As on date, it has more than 1550 beds including bassinets. It has an annual OPD attendance of more than 10 lakh patients.",
+                "surgery_name": "Arthroscopy, Spine Surgery, ACL Reconstruction Surgery, Osteotomy, etc.",
+                "city": "South Delhi, Delhi",
+                "img": "https://aniportalimages.s3.amazonaws.com/media/details/gfkmble.jpg",
+                "rate": 4.9
+        },
+        {
+                "name": "Nizam's Institute of Medical Sciences (NIMS)",
+                "description": "As of 2014, there were 900 beds, which have been increased to 1489. As of 2014, there were 111 teaching staff and by the end of 2022, it will be increased to 264. Previously, 82 resident doctors were allocated annually, but currently 169 are allocated.The pediatric and cardiology unit, 200-bed ICU, ventilators and other facilities set up at Nims Hospital at a cost of 5 crore rupees on June 23, 2022, were inaugurated by Telangana State Finance and Health MinisterT. Harish Raostarted.",
+                "surgery_name": "SURGICAL GASTROENTEROLOGY, SURGICAL ONCOLOGY, etc.",
+                "city": "hyderabad, Telangana",
+                "img": "https://www.nims.edu.in/sliderImages/1.JPG",
+                "rate": 4.8
+        },
+        {
+                "name": "Rajiv Gandhi Government General Hospital",
+                "description": "While the hospital is managed by the medical superintendent, the dean is the head of the Madras Medical College (MMC) attached to the hospital. By 2006, the hospital started treating about 8,000 to 10,000 outpatients every day. The hospital also performed three open-heart surgeries free of cost daily. By 2013, the number of outpatients per day increased to 10,000 to 12,000. The hospital contributes to the second largest number of deceased organ donations in Tamil Nadu. In March 2012, the hospital performed its 1,000th kidney transplant, the highest in any government hospital in the country, of which about 90 were cadaver transplants.",
+                "surgery_name": "Orthopedics, Pediatrics, Obstetrics and Gynecology, Ophthalmology, etc.",
+                "city": "Chennai, Tamilnadu",
+                "img": "https://www.chennaionline.com/wp-content/uploads/2023/07/govt-hospital-1200x675.jpg",
+                "rate": 4.8
+        },
+        {
+                "name": "Christian Medical College (CMC), Vellore",
+                "description": "CMC is a leading medical institution that offers a range of medical facilities, including an ambulance service, diagnostic facilities, and a medicine shop. The hospital has over 3,000 beds and a team of over 1,200 doctors.",
+                "surgery_name": "Rare blood disorders,Bone marrow transplantation, etc.",
+                "city": "Vellore, Tamilnadu",
+                "img": "https://givecmc.org/wp-content/uploads/2022/11/Ranipet-after-opened-Front-entrance-w-signs-FEATURE-image-rations-2022-11-15-06.jpg-scaled.jpg",
+                "rate": 4.7
+        },
+        {
+                "name": "Sanjay Gandhi Postgraduate Institute of Medical Sciences (SGPGIMS)",
+                "description": "SGPGIMS is a leading medical institution that offers a range of medical facilities, including an ambulance service, diagnostic facilities, and a medicine shop. The hospital has over 1,200 beds and a team of over 700 doctors.",
+                "surgery_name": "cardiology, neurology, nephrology, gastroenterology, endocrinology, hematology, medical genetics, pulmonary medicine, medical oncology, surgical oncology, urology, ophthalmology, radiology, pathology, microbiology, etc.",
+                "city": "Lucknow, Uttar Pradesh",
+                "img": "https://www.admissionfever.com/Media/clgimg/gallery/971_img8769718284808488.jpg",
+                "rate": 4.5
+        }
+        
+      
+]
+
