ef569a60de28f7b217def47b81fbb358bec4d6ca
MUKILAN019 <mukilan.p@kalvium.community>
2024-05-19 11:48:01 +0530

    Added Dummy data

---
+++ Surgical_Front_End/src/Components/details.jsx
@@ -0,0 +1,27 @@
+const details = [
+    {name:"All India Institute of Medical Sciences (AIIMS), Delhi",
+    descrpition:"AIIMS is a premier government-run medical institute in India that offers a wide range of medical facilities. The hospital has an accredited ambulance service, diagnostic facilities, medicine shop, and mediclaim is accepted. AIIMS has a wide range of departments, 1,300 beds, and a team of over 600 doctors.",
+    surgery_name:"Minimal Invasive, Breast, Endocrine, Gastrointestinal, Vascular, etc.",
+    city:"Ansari Nagar, Delhi",
+    Img:"https://static.theprint.in/wp-content/uploads/2020/07/AIIMS-2.jpg",
+    rate: 5},
+    {name:"Omandurar Hospital,Tamilnadu",
+    descrpition:"Tamil Nadu Government Multi-Super-Speciality Hospital is a 400-bed government-owned super-speciality hospital in Chennai, India. It is located at the Omandurar Government Estate on Anna Salai and was opened in February 2014",
+    surgery_name:"Procedures,Angioplasty,Coronary angiography,Carotid endarterectomy,Pulmonary lobectomy, etc.",
+    city:"Chennai, Tamilnadu",
+    Img:"https://www.shutterstock.com/shutterstock/videos/1104819279/thumb/1.jpg?ip=x480",
+    rate:4.7},
+    {name:"Jawaharlal Institute of Postgraduate Medical Education and Research (JIPMER)",
+    descrpition:"It is an institution established for teaching, research and patient care in the area of health. JIPMER spreads over 192 acres. The institution houses one administrative block, one academic centre, one nursing college, seven hospital blocks, seven accessory services buildings and four residential complexes.",
+    surgery_name:"Ophthalmology, Orthopaedics, Paediatric Surgery, Plastic Surgery, etc.",
+    city:"Puducherry",
+    Img:"https://jipmer.edu.in/sites/default/files/jipmer-entrance.jpg",
+    rate:4.8},
+    {name:"King Edward Memorial (KEM) Hospital",
+    descrpition:"The K.E.M. Hospital was the first Indian hospital housing the out-patient department within the main hospital building). The plans were submitted to W.A.Pite who had designed the Kings College Hospital in London and was then a leading authority on hospital construction.",
+    surgery_name:"Pancreatic surgery, Endocrine Surgery,  Colorectal surgery,  Breast surgery, etc.",
+    city:"Mumbai, Maharashtra",
+    Img:"https://th-i.thgim.com/public/news/cities/mumbai/addd0n/article25184732.ece/alternates/FREE_1200/11BMKEMHOSPITALSTORY",
+    rate:4.9}
+]
+export default details;
---
+++ Surgical_Front_End/src/Components/images.jsx
@@ -0,0 +1,11 @@
+import img1 from "../Data/AIIMS.avif";
+import img2 from "../Data/KEM.avif";
+import img3 from "../Data/jipmer.jpg";
+import img4 from "../Data/hospital.jpg";
+const Images={
+    img1:{img1},
+    img2:{img2},
+    img3:{img3},
+    img4:{img4},
+}
+export default Images;
